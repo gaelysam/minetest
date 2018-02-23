1dcbf5887eb4f1ad78bc07114612510c171971a4
Gael-de-Sailly <gael.chretien@akeonet.com>
2018-02-23 19:07:09 +0100

    Schematic::placeOnMap and OnVManip: fix center position Offset should be (size-1)/2 instead of (size+1)/2

--- src/mapgen/mg_schematic.cpp
+++ src/mapgen/mg_schematic.cpp
@@ -188,11 +188,11 @@ bool Schematic::placeOnVManip(MMVManip *vm, v3s16 p, u32 flags,
 
 	//// Adjust placement position if necessary
 	if (flags & DECO_PLACE_CENTER_X)
-		p.X -= (s.X + 1) / 2;
+		p.X -= (s.X - 1) / 2;
 	if (flags & DECO_PLACE_CENTER_Y)
-		p.Y -= (s.Y + 1) / 2;
+		p.Y -= (s.Y - 1) / 2;
 	if (flags & DECO_PLACE_CENTER_Z)
-		p.Z -= (s.Z + 1) / 2;
+		p.Z -= (s.Z - 1) / 2;
 
 	blitToVManip(vm, p, rot, force_place);
 
@@ -219,11 +219,11 @@ void Schematic::placeOnMap(ServerMap *map, v3s16 p, u32 flags,
 
 	//// Adjust placement position if necessary
 	if (flags & DECO_PLACE_CENTER_X)
-		p.X -= (s.X + 1) / 2;
+		p.X -= (s.X - 1) / 2;
 	if (flags & DECO_PLACE_CENTER_Y)
-		p.Y -= (s.Y + 1) / 2;
+		p.Y -= (s.Y - 1) / 2;
 	if (flags & DECO_PLACE_CENTER_Z)
-		p.Z -= (s.Z + 1) / 2;
+		p.Z -= (s.Z - 1) / 2;
 
 	//// Create VManip for effected area, emerge our area, modify area
 	//// inside VManip, then blit back.
