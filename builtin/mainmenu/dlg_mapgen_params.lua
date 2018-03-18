c020dcef698011422cdf046c3f0f3a6ec2a9bd44
Gael-de-Sailly <gael.chretien@akeonet.com>
2018-03-18 01:57:04 +0100

    Added advanced mapgen settings dialog in mapgen params dialog

--- builtin/mainmenu/dlg_mapgen_params.lua
+++ builtin/mainmenu/dlg_mapgen_params.lua
@@ -15,24 +15,40 @@
 --with this program; if not, write to the Free Software Foundation, Inc.,
 --51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+local mgname
+
 local function mapgen_params_formspec(data)
-	local mgname = data.mgname
 	local preset_list = "Original,Voxelproof,Wider,Custom"
 
 	local retval =
-		"size[11.5,4.5,true]" ..
+		"size[11.5,5.5,true]" ..
 		"label[2,0;" .. fgettext("Mapgen name") .. "]"..
 		"label[4.2,0;" .. mgname .. "]" ..
 
 		"label[2,1;" .. fgettext("Parameters preset") .. "]" ..
 		"textlist[4.2,1;5.8,2.3;presets;" .. preset_list .. ";" .. "1" .. ";false]" ..
 
-		"button[3.25,4;2.5,0.5;mapgen_params_confirm;" .. fgettext("Save") .. "]" ..
-		"button[5.75,4;2.5,0.5;mapgen_params_cancel;" .. fgettext("Cancel") .. "]"
+		"button[4.2,4;5.8,0.5;advanced_params;" .. fgettext("Advanced parameters") .. "]" ..
+
+		"button[3.25,5;2.5,0.5;mapgen_params_confirm;" .. fgettext("Save") .. "]" ..
+		"button[5.75,5;2.5,0.5;mapgen_params_cancel;" .. fgettext("Cancel") .. "]"
 		
 	return retval
 
 end
+
+local mapgen_names = {
+	v5 = "Mapgen V5",
+	v6 = "Mapgen V6",
+	v7 = "Mapgen V7",
+	valleys = "Mapgen Valleys",
+	carpathian = "Mapgen Carpathian",
+	fractal = "Mapgen Fractal",
+	flat = "Mapgen Flat",
+	singlenode = "Mapgen Singlenode",
+}
+
+local mapgen
 
 local function mapgen_params_buttonhandler(this, fields)
 	if fields["mapgen_params_confirm"] then
@@ -46,16 +62,31 @@ local function mapgen_params_buttonhandler(this, fields)
 		return true
 	end
 
+	if fields["advanced_params"] then
+		local paths = {
+			"Mapgen*",
+			"Mapgen/Biome API temperature and humidity noise parameters",
+			"Mapgen/" .. mapgen,
+		}
+
+		local adv_settings_dlg = create_adv_settings_dlg(mgname, paths)
+		adv_settings_dlg:set_parent(this)
+		this:hide()
+		adv_settings_dlg:show()
+		return true
+	end
+
 	return false
 end
 
 
-function create_mapgen_params_dlg(mgname)
+function create_mapgen_params_dlg(arg_mgname)
 	local retval = dialog_create("sp_mapgen_params",
 					mapgen_params_formspec,
 					mapgen_params_buttonhandler,
 					nil)
-	retval.data.mgname = mgname
+	mgname = arg_mgname
+	mapgen = mapgen_names[arg_mgname]
 	
 	return retval
 end
