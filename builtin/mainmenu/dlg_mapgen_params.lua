db502341ac2f6a0363389685cd90f4ed03fa69ef
Gael-de-Sailly <gael.chretien@akeonet.com>
2018-11-27 15:34:09 +0100

    Detect mapgen params files in "mapgen_params/" (Untested yet)

--- builtin/mainmenu/dlg_mapgen_params.lua
+++ builtin/mainmenu/dlg_mapgen_params.lua
@@ -15,9 +15,21 @@
 --with this program; if not, write to the Free Software Foundation, Inc.,
 --51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+local mgpresets_path = core.get_builtin_path() .. DIR_DELIM .. "mapgen_presets"
+
+local presets_list = core.get_dir_list(mgpresets_path, false)
+local preset_text_list = table.concat(presets_list, ",")
+
 local function mapgen_params_formspec(data)
 	local mgname = data.mgname
-	local preset_list = "Original,Voxelproof,Wider,Custom"
+	local presetname = core.settings:get(mg_preset)
+	local selected_preset = 1
+	for i, preset in ipairs(presets_list) do
+		if presetname == preset then
+			selected_preset = i
+			break
+		end
+	end
 
 	local retval =
 		"size[11.5,4.5,true]" ..
@@ -25,7 +37,7 @@ local function mapgen_params_formspec(data)
 		"label[4.2,0;" .. mgname .. "]" ..
 
 		"label[2,1;" .. fgettext("Parameters preset") .. "]" ..
-		"textlist[4.2,1;5.8,2.3;presets;" .. preset_list .. ";" .. "1" .. ";false]" ..
+		"textlist[4.2,1;5.8,2.3;presets;" .. preset_text_list .. ";" .. selected_preset .. ";false]" ..
 
 		"button[3.25,4;2.5,0.5;mapgen_params_confirm;" .. fgettext("Save") .. "]" ..
 		"button[5.75,4;2.5,0.5;mapgen_params_cancel;" .. fgettext("Cancel") .. "]"
