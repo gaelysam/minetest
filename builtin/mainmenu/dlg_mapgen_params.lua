b2ae19f19f4b4455b01efd857c799ef3caf8a00e
Gaël de Sailly <gael.chretien@akeonet.com>
2018-11-27 19:11:08 +0100

    Added a checkbox for displaying only the current mapgen's presets Various improvements, now working.

--- builtin/mainmenu/dlg_mapgen_params.lua
+++ builtin/mainmenu/dlg_mapgen_params.lua
@@ -17,30 +17,54 @@
 
 local mgpresets_path = core.get_builtin_path() .. DIR_DELIM .. "mapgen_presets"
 
-local presets_list = core.get_dir_list(mgpresets_path, false)
-local preset_text_list = table.concat(presets_list, ",")
+local function load_preset_list()
+	local preset_filenames = core.get_dir_list(mgpresets_path, false)
+	local presets = {}
+	for _, filename in ipairs(preset_filenames) do
+		local path = mgpresets_path .. DIR_DELIM .. filename
+		local settings = Settings(path)
+		local name = settings:get("mg_preset")
+		presets[name] = {
+			path = path,
+			mapgen = settings:get("mg_name")
+		}
+		print("Registering the preset " .. name .. " located at " .. path)
+	end
+	return presets
+end
+
+local preset_list = load_preset_list()
 
 local function mapgen_params_formspec(data)
 	local mgname = data.mgname
-	local presetname = core.settings:get(mg_preset)
+	local only_selected_mapgen = core.settings:get("mg_preset_only_selected_mapgen")
+
+	local preset_text_list = ""
+	local presetname = core.settings:get("mg_preset")
+	local i = 0
 	local selected_preset = 1
-	for i, preset in ipairs(presets_list) do
-		if presetname == preset then
-			selected_preset = i
-			break
+	for name, preset in pairs(preset_list) do
+		if not only_selected_mapgen or preset.mapgen == mgname then
+			i = i + 1
+			if name == presetname then
+				selected_preset = i
+			end
+			preset_text_list = preset_text_list .. name .. ","
 		end
 	end
+	preset_text_list = preset_text_list:sub(1,-2) -- Remove the ending comma
 
 	local retval =
-		"size[11.5,4.5,true]" ..
+		"size[11.5,6,true]" ..
 		"label[2,0;" .. fgettext("Mapgen name") .. "]"..
 		"label[4.2,0;" .. mgname .. "]" ..
 
-		"label[2,1;" .. fgettext("Parameters preset") .. "]" ..
-		"textlist[4.2,1;5.8,2.3;presets;" .. preset_text_list .. ";" .. selected_preset .. ";false]" ..
+		"checkbox[2,1;only_selected_mapgen;" .. fgettext("Show only presets of current mapgen") .. ";" .. tostring(only_selected_mapgen) .. "]" ..
+		"label[2,2;" .. fgettext("Parameters preset") .. "]" ..
+		"textlist[4.2,2;5.8,2.3;presets;" .. preset_text_list .. ";" .. selected_preset .. ";false]" ..
 
-		"button[3.25,4;2.5,0.5;mapgen_params_confirm;" .. fgettext("Save") .. "]" ..
-		"button[5.75,4;2.5,0.5;mapgen_params_cancel;" .. fgettext("Cancel") .. "]"
+		"button[3.25,5;2.5,0.5;mapgen_params_confirm;" .. fgettext("Save") .. "]" ..
+		"button[5.75,5;2.5,0.5;mapgen_params_cancel;" .. fgettext("Cancel") .. "]"
 		
 	return retval
 
